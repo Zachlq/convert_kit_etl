73f15ba684d1a460b83e5e106544bdf43ed35aab
Zach Quinn <58344148+Zachlq@users.noreply.github.com>
2023-12-03 18:32:56 -0500

    Update config.py

--- learning_sql_ck_subs/config.py
+++ learning_sql_ck_subs/config.py
@@ -2,6 +2,8 @@ dataset = "convert_kit"
 
 lsql_ck_table = "lsql_total_subs"
 
+lsql_creds = "lsql_ck_secret.json"
+
 total_subs_schema = [{
     "name": "total", "type": "INTEGER", "mode": "NULLABLE"
 },
